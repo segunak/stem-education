4d0ef4a5eb955b0fc554eeaf773261db5f182cc7
Segun <akintundejr4@gmail.com>
2024-11-07 10:07:57 -0500

    git ignore and a lot of work

---
+++ petoi-bittle/workshops/Three Levels of Programming/code/level-2.py
@@ -0,0 +1,38 @@
+# Level 2: High Level Programming
+
+from PetoiRobot import *
+
+autoConnect() # Do not remove this.
+
+# The sendSkillStr function sends a command to the robot dog to perform a skill.
+# It takes two parameters:
+# - The first parameter is the skill string (e.g., 'ksit') which tells the robot what to do.
+# - The second parameter is the delay time in seconds (e.g., 3) which determines how long the robot will wait after performing the action.
+
+# Write your code below! The first command has been written for you.
+
+########## Student Code Area Start #################
+
+# 1. Task: Make the dog sit down, then wait 3 seconds.
+sendSkillStr('ksit', 3)
+
+# 2. Task: Make the dog stand up, then wait 3 seconds. Put your code on the line below.
+
+
+# 3. Task: Make the dog perform a push-up, then wait 3 seconds. Put your code on the line below.
+
+
+# 4. Task: Make the dog jump, then wait 3 seconds. Put your code on the line below.
+
+
+# 5. Task: Make the dog do a back flip, then wait 5 seconds. Put your code on the line below.
+
+
+# 6. Task: Make the dog do a front flip, then wait 5 seconds. Put your code on the line below.
+
+
+# 7. Task: Make the dog rest, then wait 1 second. Put your code on the line below.
+
+########## Student Code Area End #################
+
+closePort() # Do not remove this.
